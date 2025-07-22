3f1015cc8031ac1b88e118afa799d0864f2dd8c8
Bryan Hoda <bryanhoda2003@gmail.com>
2025-07-22 18:31:06 +0530

    made coin page responsive as it was easy lol

--- src/components/CoinPage.jsx
+++ src/components/CoinPage.jsx
@@ -19,48 +19,51 @@ const CoinPage = () => {
 
     return (
         <>
-            <div className="wrapper flex items-center justify-center gap-5 w-full text-white p-6 mx-auto">
+            <div className="wrapper flex flex-col lg:flex-row items-center justify-center gap-5 w-full max-w-6xl text-white lg:p-6 mx-auto">
                 {coinData.image && (
-                    <img src={coinData.image?.large} alt={coinData.name} className="w-[30%] h-auto mb-6" />
+                    <img src={coinData.image?.large} alt={coinData.name} className="w-full sm:w-1/2 lg:w-[20%] h-auto mb-6" />
                 )}
 
-                <div className="info text-white p-6 w-[70%] h-auto mx-auto">
+                <div className="info text-white w-full sm:w-[80%] lgw-[60%] h-auto max-sm:my-3 px-3 sm:px-5 py-5 bg-[#0d1b2a] shadow-xl rounded-lg">
                     <h1 className="text-3xl font-bold mb-2">{coinData.name}</h1>
                     <div className="text-gray-400 uppercase mb-4">{coinData.symbol}</div>
 
-                    <span className="font-semibold text-yellow-400">
+                    <span className="font-semibold text-yellow-400 pb-3">
                         Market Rank : #{coinData.market_cap_rank || "N/A"}
                     </span>
 
-                    <div className="my-4">
-                        <span className="font-semibold text-gray-300 text-">Description:
-                            {coinData.description && (
-                                <p className="mt-1 w-[80%]"
-                                    dangerouslySetInnerHTML={{
-                                        __html: coinData.description.en?.split('. ')[0] + '.',
-                                    }} />
-                            )}
-                        </span>
-
-                        <ul className="flex flex-col text-sm gap-5 my-4 mt-5">
-                            <li>Current Price : <span>{coinData.market_data?.current_price?.usd
-                                ? `$${coinData.market_data.current_price.usd.toLocaleString()}`
-                                : "N/A"}</span></li>
-
-                            <li>Highest Value (last 24h) : <span>{coinData.market_data?.high_24h?.usd
-                                ? `$${coinData.market_data.high_24h.usd.toLocaleString()}`
-                                : "N/A"}</span></li>
-
-                            <li>Lowest Value (last 24h) : <span>{coinData.market_data?.low_24h?.usd
-                                ? `$${coinData.market_data.low_24h.usd.toLocaleString()}`
-                                : "N/A"}</span></li>
-
-                            <li>Total Supply : <span>{coinData.market_data?.total_supply ? `${coinData.market_data?.total_supply.toLocaleString()}` : "N/A"}</span></li>
-                            <li>Max Supply : <span>{coinData.market_data?.max_supply ? `${coinData.market_data?.max_supply.toLocaleString()}` : "N/A"}</span></li>
-                        </ul>
-
-                        <p className="pt-5 text-gray-300 text-sm">Last Updated on - {coinData.last_updated?.split("T")[0] || "N/A"}</p>
+                    <div className="font-semibold py-4 text-gray-300">Description:
+                        {coinData.description && (
+                            <p className="mt-1 w-full lg:w-[80%]"
+                                dangerouslySetInnerHTML={{
+                                    __html: coinData.description.en?.split('. ')[0] + '.',
+                                }} />
+                        )}
                     </div>
+
+                    <ul className="grid grid-cols-1 md:grid-cols-2 w-[80%] text-sm gap-4 py-4">
+                        <li>Current Price : <span className="text-gray-300">{coinData.market_data?.current_price?.usd
+                            ? `$${coinData.market_data.current_price.usd.toLocaleString()}`
+                            : "N/A"}</span></li>
+
+                        <li>Highest Value (last 24h) : <span className="text-gray-300">{coinData.market_data?.high_24h?.usd
+                            ? `$${coinData.market_data.high_24h.usd.toLocaleString()}`
+                            : "N/A"}</span></li>
+
+                        <li>Lowest Value (last 24h) : <span className="text-gray-300">{coinData.market_data?.low_24h?.usd
+                            ? `$${coinData.market_data.low_24h.usd.toLocaleString()}`
+                            : "N/A"}</span></li>
+
+                        <li>Total Supply : <span className="text-gray-300">{coinData.market_data?.total_supply ? `${coinData.market_data?.total_supply.toLocaleString()}` : "N/A"}</span></li>
+                        <li>Max Supply : <span className="text-gray-300">{coinData.market_data?.max_supply ? `${coinData.market_data?.max_supply.toLocaleString()}` : "N/A"}</span></li>
+                    </ul>
+
+                    <p className="pt-5 text-gray-300 text-sm">Last Updated on ~ {coinData.last_updated &&
+                        new Date(coinData.last_updated).toLocaleDateString("en-GB", {
+                            day: "numeric",
+                            month: "long",
+                            year: "numeric",
+                        })}</p>
                 </div>
             </div>
         </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom"
 const Navbar = () => {
     return (
         <>
-            <nav className="p-6 my-3 flex justify-between items-center text-white bg-[#0d1b2a] shadow-xl rounded-lg">
-                <Link to='/' className="logo text-2xl font-semibold">Crypto Dashboard</Link>
+            <nav className="p-6 sm:my-3 flex max-sm:flex-col max-sm:gap-3 justify-between items-center text-white bg-[#0d1b2a] shadow-xl sm:rounded-lg">
+                <Link to='/' className="logo text-lg sm:text-2xl font-semibold">Crypto Dashboard</Link>
 
                 <div className="warning">
-                    <p className='opacity-55'>*All the prices and info mentioned are up to date.</p>
+                    <p className='opacity-55 text-center max-sm:text-xs'>*All the prices and info mentioned are up to date.</p>
                 </div>
             </nav>
         </>
